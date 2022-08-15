71297e0feec66462aa505ffe03186275f159b10f
Hanna Agersborg Aanjesen <h.aanjesen@maxxton.com>
2022-08-15 10:26:54 +0200

    fix cordoning

--- pkg/descheduler/strategies/nodeutilization/highnodeutilization.go
+++ pkg/descheduler/strategies/nodeutilization/highnodeutilization.go
@@ -21,6 +21,7 @@ import (
 	"encoding/json"
 	"fmt"
 
+	nodev1 "k8s.io/api/node/v1"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/types"
@@ -160,7 +161,7 @@ func cordonNode(ctx context.Context, client clientset.Interface, node *v1.Node) 
 	patch.Spec.Unschedulable = true
 
 	patchJson, _ := json.Marshal(patch)
-	return client.NodeV1().RESTClient().Patch(types.MergePatchType).Name(node.Name).Body(patchJson).Do(ctx).Error()
+	return client.NodeV1().RESTClient().Patch(types.StrategicMergePatchType).Name(node.Name).Body(patchJson).Do(ctx).Error()
 
 }
 
